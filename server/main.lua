4bc0287f459ce7507a05e50d6580d65f29244d6f
bluupie <87111944+bluupie@users.noreply.github.com>
2021-09-08 09:17:50 -0700

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,5 @@
-local poor = 10000
+local flyin = 5000 -- minimum bank value to be taxed in this bracket
+local poor = 10000 -- minimum bank value to be taxed in this bracket
 local notbad = 100000 -- mininum bank value to be taxed in this bracket
 local medium = 300000 -- mininum bank value to be taxed in this bracket
 local rich = 700000 -- mininum bank value to be taxed in this bracket
@@ -17,8 +18,11 @@ AddEventHandler("qb-taxing:server:Display", function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local bankamount = Player.PlayerData.money['bank']
-    local bracket = flyin
-    if bankamount > poor and bankamount <= notbad then
+    local bracket = {}
+    if bankamount > flyin and bankamount <= poor then
+        bracket = flyin
+        percentage = "0%"
+    elseif bankamount > poor and bankamount <= notbad then
         bracket = poor
         percentage = "1%"
     elseif bankamount > notbad and bankamount <= medium then
@@ -34,7 +38,7 @@ AddEventHandler("qb-taxing:server:Display", function()
         bracket = toorich
         percentage = "8%"
     end
-    TriggerClientEvent("QBCore:Notify", src, "Your Current Tax Bracket is " ..bracket.. " Tax Percentage is " ..percentage.. "!")
+    TriggerClientEvent("QBCore:Notify", src, "Your Current Tax Bracket is $" ..bracket.. " Tax Percentage is " ..percentage.. "!")
 end)
 
 RegisterServerEvent('qb-taxing:server:paytaxes')
@@ -49,36 +53,29 @@ AddEventHandler('qb-taxing:server:paytaxes', function(amount)
     local tax4 = (bankamount / 100 * 8) -- change for percentage/tax bracket
     
     if bankamount <= poor then
-      --Player.Functions.RemoveMoney('bank', tax, "Taxes paid")
       TriggerClientEvent('QBCore:Notify', source, "You do not have enough money to be taxed" , "error")
       --TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax.."in taxes.")
     elseif bankamount > poor and bankamount <= notbad then
         Player.Functions.RemoveMoney('bank', tax, "Taxes paid")
-        --TriggerClientEvent('QBCore:Notify', source, "You have paid $" ..tax1.. " your taxes off" , "success")
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax)
         TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
 
     elseif bankamount > notbad and bankamount <= medium then
         Player.Functions.RemoveMoney('bank', tax1, "Taxes paid")
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax1)
-        --TriggerClientEvent('QBCore:Notify', source, "You have paid $" ..tax1.. " your taxes off" , "success")
         TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
     
     elseif bankamount > medium and bankamount <= rich then
         Player.Functions.RemoveMoney('bank', tax2, "Taxes paid")
-        TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax2)
-        --TriggerClientEvent('QBCore:Notify', source, "You have paid $" ..tax2.. " your taxes off" , "success")
         TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax2.."in taxes.")
 
     elseif bankamount > rich and bankamount <= toorich then
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax3)
-        --TriggerClientEvent('QBCore:Notify', source, "You have paid $" ..tax2.. " your taxes off" , "success")
         TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax3.."in taxes.")
     
     elseif bankamount > toorich then
         Player.Functions.RemoveMoney('bank', tax4, "Taxes paid")
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax4)
-        --TriggerClientEvent('QBCore:Notify', source, "You have paid $" ..tax3.. " your taxes off" , "success")
         TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax4.."in taxes.")
     end
 end)
