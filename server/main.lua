7291a27a6f0f22a244dd8e720b672386f0335125
JulyGee <87626039+JulyGee@users.noreply.github.com>
2021-11-25 17:34:07 -0500

    Added the ability to have all the money collected deposited into a Boss Account!
    * Added the ability to have all the money collected deposited into a Boss Account!
        - To Change Job that gets payment just replace every "government" in the server.lua to your desired job.

--- server/main.lua
+++ server/main.lua
@@ -5,6 +5,8 @@ local medium = 300000 -- mininum bank value to be taxed in this bracket
 local rich = 700000 -- mininum bank value to be taxed in this bracket
 local toorich = 1000000 -- mininum bank value to be taxed in this bracket
 local percentage = {}
+local QBCore = exports['qb-core']:GetCoreObject()
+
 --     flyinperc = 0,
 --     poorperc = 1,
 --     notbadperc = 2,
@@ -54,28 +56,35 @@ AddEventHandler('qb-taxing:server:paytaxes', function(amount)
     
     if bankamount <= poor then
       TriggerClientEvent('QBCore:Notify', source, "You do not have enough money to be taxed" , "error")
-      --TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax.."in taxes.")
+      TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax.."in taxes.")
     elseif bankamount > poor and bankamount <= notbad then
         Player.Functions.RemoveMoney('bank', tax, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
 
     elseif bankamount > notbad and bankamount <= medium then
         Player.Functions.RemoveMoney('bank', tax1, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax1)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax1)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
     
     elseif bankamount > medium and bankamount <= rich then
         Player.Functions.RemoveMoney('bank', tax2, "Taxes paid")
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax2.."in taxes.")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax2)
+        TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax2)
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax2.."in taxes.")
 
     elseif bankamount > rich and bankamount <= toorich then
+        Player.Functions.RemoveMoney('bank', tax3, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax3)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax3)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax3.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax3.."in taxes.")
     
     elseif bankamount > toorich then
         Player.Functions.RemoveMoney('bank', tax4, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax4)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax4)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax4.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax4.."in taxes.")
     end
 end)
