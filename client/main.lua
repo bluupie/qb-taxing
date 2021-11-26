ab5e2acceeea0acf2ffa97b63165b0558828e554
CptNoHand <B_Marutschke@gmx.de>
2021-11-26 17:12:37 +0100

    refactor

--- client/main.lua
+++ client/main.lua
@@ -4,7 +4,7 @@ TriggerEvent('QBCore:getObject', function(obj) QBCore = obj end)
 
 Citizen.CreateThread(function()
 	while true do
-		Wait(4*900000) -- change time of payment // current time is 45 minutes
+		Wait(4*675000) -- change time of payment // current time is 45 minutes
 		TriggerServerEvent('qb-taxing:server:paytaxes')
 	end
 end)
--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,3 @@
-local QBCore = exports['qb-core']:GetCoreObject()
 local flyin = 5000 -- minimum bank value to be taxed in this bracket
 local poor = 10000 -- minimum bank value to be taxed in this bracket
 local notbad = 100000 -- mininum bank value to be taxed in this bracket
@@ -6,6 +5,8 @@ local medium = 300000 -- mininum bank value to be taxed in this bracket
 local rich = 700000 -- mininum bank value to be taxed in this bracket
 local toorich = 1000000 -- mininum bank value to be taxed in this bracket
 local percentage = {}
+local QBCore = exports['qb-core']:GetCoreObject()
+
 --     flyinperc = 0,
 --     poorperc = 1,
 --     notbadperc = 2,
@@ -31,13 +32,13 @@ AddEventHandler("qb-taxing:server:Display", function()
         percentage = "2%"
     elseif bankamount > medium and bankamount <= rich then
         bracket = medium
-        percentage = "3%"
+        percentage = "4%"
     elseif bankamount > rich and bankamount <= toorich then
         bracket = rich
-        percentage = "4%"
+        percentage = "6%"
     elseif bankamount > toorich then
         bracket = toorich
-        percentage = "5%"
+        percentage = "8%"
     end
     TriggerClientEvent("QBCore:Notify", src, "Your Current Tax Bracket is $" ..bracket.. " Tax Percentage is " ..percentage.. "!")
 end)
@@ -49,34 +50,41 @@ AddEventHandler('qb-taxing:server:paytaxes', function(amount)
     local bankamount = Player.PlayerData.money.bank
     local tax = (bankamount / 100 * 1) -- change for percentage/tax bracket
     local tax1 = (bankamount / 100 * 2) -- change for percentage/tax bracket
-    local tax2 = (bankamount / 100 * 3) -- change for percentage/tax bracket
-    local tax3 = (bankamount / 100 * 4) -- change for percentage/tax bracket
-    local tax4 = (bankamount / 100 * 5) -- change for percentage/tax bracket
+    local tax2 = (bankamount / 100 * 4) -- change for percentage/tax bracket
+    local tax3 = (bankamount / 100 * 6) -- change for percentage/tax bracket
+    local tax4 = (bankamount / 100 * 8) -- change for percentage/tax bracket
     
     if bankamount <= poor then
       TriggerClientEvent('QBCore:Notify', source, "You do not have enough money to be taxed" , "error")
-      --TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax.."in taxes.")
+      TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax.."in taxes.")
     elseif bankamount > poor and bankamount <= notbad then
         Player.Functions.RemoveMoney('bank', tax, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
 
     elseif bankamount > notbad and bankamount <= medium then
         Player.Functions.RemoveMoney('bank', tax1, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax1)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax1)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax1.."in taxes.")
     
     elseif bankamount > medium and bankamount <= rich then
         Player.Functions.RemoveMoney('bank', tax2, "Taxes paid")
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax2.."in taxes.")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax2)
+        TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax2)
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax2.."in taxes.")
 
     elseif bankamount > rich and bankamount <= toorich then
+        Player.Functions.RemoveMoney('bank', tax3, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax3)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax3)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax3.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax3.."in taxes.")
     
     elseif bankamount > toorich then
         Player.Functions.RemoveMoney('bank', tax4, "Taxes paid")
+        TriggerEvent("qb-bossmenu:server:addAccountMoney", "government", tax4)
         TriggerClientEvent("qb-taxing:client:SendTaxMail", src, tax4)
-        TriggerEvent("qb-log:server:CreateLog", "banking", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax4.."in taxes.")
+        TriggerEvent("qb-log:server:CreateLog", "taxes", "Taxes", "red", "**"..GetPlayerName(src) .. "** has paid $"..tax4.."in taxes.")
     end
 end)
